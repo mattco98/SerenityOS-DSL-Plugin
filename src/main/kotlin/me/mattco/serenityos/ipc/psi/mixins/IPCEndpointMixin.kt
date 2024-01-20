a7ac658cb3df7d70a3230787f4e43d84a15b8cc6
Matthew Olsson <matthewcolsson@gmail.com>
2024-01-20 12:32:24 -0700

    IPC: Resolve C++ endpoint classes

---
+++ src/main/kotlin/me/mattco/serenityos/ipc/psi/mixins/IPCEndpointMixin.kt
@@ -0,0 +1,51 @@
+package me.mattco.serenityos.ipc.psi.mixins
+
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiManager
+import com.intellij.psi.search.FileTypeIndex
+import com.intellij.psi.util.PsiTreeUtil
+import com.jetbrains.cidr.lang.OCFileType
+import com.jetbrains.cidr.lang.psi.OCElement
+import com.jetbrains.cidr.lang.psi.OCFile
+import com.jetbrains.cidr.lang.psi.OCStructLike
+import com.jetbrains.cidr.lang.search.scopes.OCSearchScope
+import me.mattco.serenityos.ipc.psi.IPCNamedElement
+import me.mattco.serenityos.ipc.psi.api.IPCEndpoint
+import me.mattco.serenityos.ipc.psi.multiRef
+
+abstract class IPCEndpointMixin(node: ASTNode) : IPCNamedElement(node), IPCEndpoint {
+    override fun getReference() = multiRef { resolveCppDecl() }
+
+    private fun resolveCppDecl(): List<OCElement> {
+        val scope = OCSearchScope.getProjectSourcesScope(project)
+        val psiManager = PsiManager.getInstance(project)
+
+        // We will only ever resolve to header files. Additionally, IPC files have some naming
+        // conventions that we can use to avoid searching thousands of files
+        val files = FileTypeIndex.getFiles(OCFileType.INSTANCE, scope)
+            .filter {
+                val name = it.nameWithoutExtension
+                it.extension == "h" && ("Client" in name || "Server" in name || "Connection" in name)
+            }
+
+        val matchingEndpoints = mutableListOf<OCStructLike>()
+
+        for (file in files) {
+            val cppFile = psiManager.findFile(file) as OCFile
+            val structs = PsiTreeUtil.findChildrenOfType(cppFile, OCStructLike::class.java)
+            for (struct in structs) {
+                for (base in struct.baseClausesList?.baseClauses.orEmpty()) {
+                    val baseText = base.text
+                    if (endpointName !in baseText) continue
+                    if (baseText.startsWith("public IPC::ConnectionFromClient")
+                        || baseText.startsWith("public IPC::ConnectionToServer")
+                    ) {
+                        matchingEndpoints.add(struct)
+                    }
+                }
+            }
+        }
+
+        return matchingEndpoints
+    }
+}
